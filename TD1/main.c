0dda0b09e5ed7ac37e0d5aae3a2ed319640fe389
Lear-kao <axelaudeoud66@gmail.com>
2025-03-13 15:21:37 +0100

    klmù

--- TD1/main.c
+++ TD1/main.c
@@ -11,6 +11,7 @@ int main( void )
     if(a == 0)
     {
         pgm *image = pgm_read_bin("data/eye_s_asc.pgm");
+        pgm_to_jpeg(image,"aled.jpg");
         jpeg_to_pgm(image,"nice.jpg");
         return 0;
     }
